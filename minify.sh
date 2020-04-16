8072a419138ae495e9e58975fbb37517f7dcc5dd
Kaarel Pärtel <kaarelp2rtel@gmail.com>
2020-04-16 16:32:34 +0300

    Add html minification
    Minifying html id and class names with sed

--- minify.sh
+++ minify.sh
@@ -1,5 +1,40 @@
 #!/bin/bash
 
 for file in $(echo src/*); do
-    minify "$file" > $(basename "$file")
+    target=$(basename "$file")
+    minify "$file" > "$target"
+    sed -i 's/tutor-window/tw/g' "$target"
+    sed -i 's/tutor-container/tc/g' "$target"
+    sed -i 's/tutor-completed/tm/g' "$target"
+    sed -i 's/tutor-cursor/tr/g' "$target"
+    sed -i 's/tutor-uncompleted/tu/g' "$target"
+    sed -i 's/tutor-result/te/g' "$target"
 done 
+
+# .tutor-window{
+#     width: 75vw;
+#     padding: 1vh;
+#     position: absolute;
+#     top: 50%;
+#     left: 50%;
+#     transform: translate(-50%, -50%);
+#     font-family: 'Courier New', Courier, monospace;
+# }
+# .tutor-container{
+#     font-size: 0;
+#     display: inline;
+#     white-space: nowrap;
+# }
+# .tutor-completed{
+#     color: gray;
+#     font-size: 4vh;
+#     white-space: pre-wrap;
+# }
+# .tutor-cursor{
+#     color: red;
+#     font-size: 4vh;
+#     text-decoration: underline;
+#     white-space: pre-wrap;
+
+# }
+# .tutor-uncompleted{
