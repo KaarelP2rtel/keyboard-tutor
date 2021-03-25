9d970a4253f24c1163999ad98326ad24f96ab21b
Kaarel Pärtel <kaarel.partel@milrem.com>
2021-03-25 11:01:50 +0200

    Refactor code
    Saves about 100 bytes after compression

--- minify.sh
+++ minify.sh
@@ -3,26 +3,14 @@
 for file in $(echo src/*); do
     target=$(basename "$file")
     minify "$file" > "$target"
-    sed -i 's/tutor-window/tw/g' "$target"
-    sed -i 's/tutor-container/tc/g' "$target"
-    sed -i 's/tutor-completed/tm/g' "$target"
-    sed -i 's/tutor-cursor/tr/g' "$target"
-    sed -i 's/tutor-uncompleted/tu/g' "$target"
-    sed -i 's/tutor-result/te/g' "$target"
-    sed -i 's/tutor-status/ts/g' "$target"
-    sed -i 's/current-layout/cl/g' "$target"
-
-    sed -i 's/KEYS/K/g' "$target"
-    sed -i 's/BACKSPACE/B/g' "$target"
-    sed -i 's/SHIFT/S/g' "$target"
-    sed -i 's/SPACE/C/g' "$target"
-    sed -i 's/KEY_O/N/g' "$target"
-    sed -i 's/KEY_Z/Z/g' "$target"
-    sed -i 's/COMMA/M/g' "$target"
-    sed -i 's/DASH/D/g' "$target"
-    sed -i 's/PERIOD/P/g' "$target"
-    sed -i 's/FORWARD_SLASH/F/g' "$target"
-
+    sed -i 's/tutor-window/a/g' "$target"
+    sed -i 's/tutor-container/b/g' "$target"
+    sed -i 's/tutor-completed/c/g' "$target"
+    sed -i 's/tutor-cursor/d/g' "$target"
+    sed -i 's/tutor-uncompleted/e/g' "$target"
+    sed -i 's/tutor-result/f/g' "$target"
+    sed -i 's/tutor-status/g/g' "$target"
+    sed -i 's/current-layout/h/g' "$target"
 done
 
 
